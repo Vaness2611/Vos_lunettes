ee34c7d46eea9ca11a5b318802eb018cee3d809e
Vaness <nchovaness@gmail.com>
2022-10-03 11:27:21 +0000

    ajout de mon projet

---
+++ index.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="en" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Site de montures">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Vos lunettes</title>
+    <link rel="shortcut icon" href="photos/favicon.ico" type="image/x-icon">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/cover/">
+
+    
+
+    <!-- Bootstrap core CSS -->
+<link href="bootstrap.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="cover.css" rel="stylesheet">
+  </head>
+  <body class="d-flex h-100 text-center text-white bg-dark">
+    
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Bienvenue</h3>
+      <nav class="nav nav-masthead justify-content-center float-md-end">
+        <a class="nav-link active" aria-current="page" href="catalogue.php">Catalogue</a>
+      </nav>
+    </div>
+  </header>
+ 
+  <main class="px-3">
+    <h1>Bonjour et bienvenue sur notre page internet</h1>
+    <p class="lead">Nous espérons que vous trouverez une monture à votre goût parmi notre large gamme de monture.</br>
+      Pour pouvoir consulter nos différentes montures, cliquez sur le lien suivant !
+    </p>
+    <p class="lead">
+      <a href="catalogue.php" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Voir le catalogue</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Axel optique - Magasin de lunettes et lunettes de soleil, Cliquez ici <a href="https://www.facebook.com/profile.php?id=100063658191552" class="text-white">@Axel_optique</a>.</p>
+  </footer>
+</div>
+
+
+    
+  </body>
+</html>
